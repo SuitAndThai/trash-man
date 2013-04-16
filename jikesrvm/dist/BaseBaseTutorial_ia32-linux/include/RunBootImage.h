f6cce16b96ce1949908c9989f2ebf093e6e9e8b6
Richard Thai <thairp@rose-hulman.edu>
2013-04-16 15:23:38 -0400

    initial commitment, ive never done this before. dont hurt me

---
+++ jikesrvm/dist/BaseBaseTutorial_ia32-linux/include/RunBootImage.h
@@ -0,0 +1,8 @@
+
+      const unsigned heap_default_initial_size = 20*1024*1024;
+      const unsigned heap_default_maximum_size = 100*1024*1024;
+      const char *rvm_version = "Jikes RVM 3.1.2 (revision )";
+      const char *rvm_configuration = "BaseBaseTutorial";
+      const char *rvm_host_configuration = "/home/csse/jikesrvm-3.1.2-thairp/build/configs/BaseBaseTutorial.properties";
+      const char *rvm_target_configuration = "/home/csse/jikesrvm-3.1.2-thairp/build/targets/ia32-linux.properties";
+    
